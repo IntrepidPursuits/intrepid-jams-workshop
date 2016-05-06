a59d2ee1e87f5793ae0bacd58fd1fcbd38f97195
Helen Hood <helen.c.hood@gmail.com>
2016-05-06 13:52:42 -0400

    Seed games

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+games = [{ teams: [{ name: 'Bulls', player_names: 'Danny, Benn', score: 812 },
+                   { name: 'Spurs', player_names: 'Logan, Ben', score: 56 }] },
+         { teams: [{ name: 'Celtics', player_names: 'Brian, Paul', score: 34 },
+                   { name: 'Spurs', player_names: 'Rachel, Helen', score: 10000 }] },
+         { teams: [{ name: 'Lakers', player_names: 'Nick, Liz', score: 92 },
+                   { name: 'Knicks', player_names: 'Bryant, Dave', score: 786 }] }]
+
+puts 'Seeding 3 games...'
+
+games.each do |game_attrs|
+  game = Game.create
+  game_attrs[:teams].each do |team_attrs|
+    team = Team.new(team_attrs)
+    team.game = game
+    team.save
+  end
+end
+
+puts 'Done.'
